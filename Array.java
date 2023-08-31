51b05a86c9a6f0cb45301b3e80d081222458073a
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-31 21:09:30 +0200

    2D Arrays

--- Array.java
+++ Array.java
@@ -3,12 +3,17 @@ import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
         // define the array with the number of memory allocated
-        int[] numbers = { 2, 3, 4, 2, 5454, 2323, 545 };
+        // int[] numbers = { 2, 3, 4, 2, 5454, 2323, 545 };
 
         // sort our arrays
-        Arrays.sort(numbers);
+        // Arrays.sort(numbers);
 
         // print out the variables
-        System.out.println(Arrays.toString(numbers));
+        // System.out.println(Arrays.toString(numbers));
+
+        // two dimensional arrays
+
+        int[][] numbers = { { 2, 4, 5 }, { 7, 5, 3 } };
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
