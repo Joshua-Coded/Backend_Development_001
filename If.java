57d279a51fb0865da4f43b8f5127ab8b89a2f7aa
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-02 17:06:42 +0200

    if

---
+++ If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("a is greater than b");
+        }
+
+        System.out.println("B is greater than a");
+    }
+}
