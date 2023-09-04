12f4de12e56fd16ee7aa7339c73370283190f71b
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-04 23:12:55 +0200

    task 6 second solution

---
+++ Space.java
@@ -0,0 +1,8 @@
+public class Space {
+    public static void main(String[] args) {
+        String str = "alana";
+
+        for (int i = 0; i <= str.length() - 1; i++)
+            System.out.print(str.charAt(i) + " ");
+    }
+}
