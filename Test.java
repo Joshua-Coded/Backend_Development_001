e77173de5d63389dda7f2066d98f26e04c3174ba
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-05 10:53:01 +0200

    pascal kind of problem

---
+++ Test.java
@@ -0,0 +1,13 @@
+// methodss
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(sum(2, 3));
+
+    }
+
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+}
