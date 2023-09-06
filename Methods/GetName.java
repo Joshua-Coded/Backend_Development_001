6e117f74fadf6294d8a9de3e8cb0badd3406c822
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-06 03:06:56 +0200

    methods

---
+++ Methods/GetName.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GetName {
+    public static void main(String[] args) {
+
+        System.out.print("Enter name and Age: ");
+        System.out.println(getName() + " " + " is " + getAge());
+
+    }
+
+    public static String getName() {
+        Scanner input = new Scanner(System.in);
+
+        String name = input.nextLine();
+        return name;
+    }
+
+    public static double getAge() {
+        Scanner num = new Scanner(System.in);
+        double s = num.nextDouble();
+        return s;
+    }
+}
