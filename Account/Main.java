93ae1e8d73fe18828af7923d23165ed54905ff03
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-07 16:08:39 +0200

    the account class defined

--- Account/Main.java
+++ Account/Main.java
@@ -1,8 +1,25 @@
+import java.awt.*;
 
 // Banking system OOP
 
 public class Main {
     public static void main(String[] args) {
+
+        // instantiate the object
+        Account testAccount = new Account(2222, 30000, 5.3);
+
+        if (testAccount.withdraw(2000)) {
+            System.out.println("Withdrawal SuccessFul!");
+        } else {
+            System.out.println("withdrawal Failed!");
+        }
+
+        // make deposit into the account
+
+        testAccount.deposit(3000);
+
+        System.out.println(testAccount.getId() + " " + testAccount.getBalance() + " "
+                + testAccount.getAnnualInterestRate() + " " + testAccount.getDateCreated());
 
     }
 }
