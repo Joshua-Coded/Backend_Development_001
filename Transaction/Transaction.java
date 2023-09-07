a629c11139a5161604b16c1e6b4b355dce0dd517
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-07 17:05:04 +0200

    the account class defined

---
+++ Transaction/Transaction.java
@@ -0,0 +1,26 @@
+
+// TRANSACTION CLASS
+import java.util.Date;
+
+public class Transaction {
+
+    // get the attributes of the transaction
+    private char type;
+    private double amount;
+    private double balance;
+    private Date date;
+    private String description;
+
+    // get the constructor
+
+    public Transaction(char type, double amount, double balance, String description) {
+        this.type = type;
+        this.amount = amount;
+        this.balance = balance;
+        this.description = description;
+
+        // instantiate the date
+
+        this.date = new Date();
+    }
+}
