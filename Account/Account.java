bb8d7276de722edcd88f5c2dd8735b9f8c2552b0
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-07 15:57:42 +0200

    the account class defined

---
+++ Account/Account.java
@@ -0,0 +1,71 @@
+import java.util.Date;
+
+public class Account {
+
+    // Account Attributes
+
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+
+    // constructors for Account
+
+    public Account(int id, double balance, double annualInterestRate) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+
+        // instantiate the date created
+
+        dateCreated = new Date();
+    }
+
+    // the withdraw method.
+
+    public boolean withdraw(double amount) {
+        if (balance > amount) {
+            balance -= amount;
+            return true;
+        }
+
+        return false;
+    }
+
+    // the deposit method.
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    // getter and setters for our properties
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+}
---
+++ Account/Main.java
@@ -0,0 +1,8 @@
+
+// Banking system OOP
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
