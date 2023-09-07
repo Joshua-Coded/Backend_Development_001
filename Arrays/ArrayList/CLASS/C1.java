7b157118dbee99620f67199ee4a8c320676ee1ce
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-07 13:56:00 +0200

    first main and class

---
+++ Arrays/ArrayList/CLASS/C1.java
@@ -0,0 +1,16 @@
+class C1 {
+
+    // public attributes
+    private int x;
+
+    // setter and getter contructor for accessing the private attributes
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+}
