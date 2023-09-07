160ad68e99c147afa270f2c84c34da1e5a74c5cb
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-07 15:19:39 +0200

    oop in action

--- Arrays/ArrayList/CLASS/Main.java
+++ Arrays/ArrayList/CLASS/Main.java
@@ -1,5 +1,37 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // C1().getX();
+        Rectangle[] rectangles = new Rectangle[3];
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < rectangles.length; i++) {
+            System.out.print("Do you want to enter the width and the height? (y/n): ");
+
+            char choice = input.next().charAt(0);
+
+            if (choice == 'y') {
+                System.out.print("Enter the width and height of a rectangle: ");
+                rectangles[i] = new Rectangle(input.nextDouble(), input.nextDouble());
+            }
+
+            else if (choice == 'n') {
+                rectangles[i] = new Rectangle();
+            }
+
+            else {
+                System.out.println("invalid choice");
+                rectangles[i] = new Rectangle();
+            }
+        }
+
+        System.out.println("The rectangles are: ");
+
+        for (int i = 0; i < rectangles.length; i++) {
+            System.out.println("Rectangle " + (i + 1) + ": width: " + rectangles[i].getWidth() + ", height: "
+                    + rectangles[i].getHeight());
+        }
     }
 }
