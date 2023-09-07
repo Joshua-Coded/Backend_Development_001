ec8d804616bd799b860f9f5d8d58894afbb74f82
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-07 17:20:04 +0200

    the account class defined

---
+++ Transaction/Account.java
@@ -0,0 +1,94 @@
+import java.util.Date;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Account {
+
+    // Account Attributes
+
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+    private Client client;
+    private ArrayList<Transaction> transactions;
+
+    // constructors for Account
+
+    public Account(int id, double balance, double annualInterestRate, Client client) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+        this.client = client;
+
+        // instantiate the transaction array list
+        this.transactions = new ArrayList<>();
+
+        // instantiate the date created
+        dateCreated = new Date();
+    }
+
+    // the withdraw method.
+
+    public boolean withdraw(double amount) {
+        if (balance > amount) {
+            balance -= amount;
+            this.transactions.add(new Transaction('W', amount, this.balance, "Withdrawn " + amount));
+            return true;
+        }
+
+        return false;
+    }
+
+    // the deposit method.
+
+    public void deposit(double amount) {
+        balance += amount;
+        this.transactions.add(new Transaction('D', amount, this.balance, "Deposited " + amount));
+    }
+
+    // getter and setters for our properties
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    // the getter an setter for our client
+
+    public Client gClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String toString() {
+        return this.id + " " + this.balance + " " + this.annualInterestRate + " " + this.dateCreated;
+    }
+
+}
---
+++ Transaction/Client.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.awt.*;
+
+public class Client {
+
+    // the client attributes
+    private int id;
+    private String name;
+    private String phone;
+    private ArrayList<Account> accounts;
+
+    // the constructors
+
+    public Client(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+
+        // instantiate the array list
+
+        accounts = new ArrayList<>();
+    }
+
+    public boolean addAccount(Account account) {
+        accounts.add(account);
+        return true;
+    }
+
+    public boolean removeAccount(int accountId) {
+        for (Account account : accounts) {
+            if (account.getId() == accountId) {
+                accounts.remove(account);
+                return true;
+            }
+
+        }
+        return false;
+    };
+
+    // the toString method
+    public String toString() {
+        String s = this.id + " " + this.name + " " + this.phone + "\n";
+
+        for (Account account : accounts) {
+            s += account.toString() + "\n";
+        }
+        return s;
+    }
+
+    // the setters and the getters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
