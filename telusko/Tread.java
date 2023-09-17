8b0b1aa127ee3378a7c91ef60a4c260e84e43e25
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-17 17:59:08 +0200

    threading for multitasking

--- telusko/Tread.java
+++ telusko/Tread.java
@@ -1,4 +1,4 @@
-class A extends Thread {
+class A implements Runnable {
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println("hi");
@@ -6,7 +6,7 @@ class A extends Thread {
     }
 }
 
-class B extends Thread {
+class B implements Runnable {
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println("hello world");
@@ -16,10 +16,13 @@ class B extends Thread {
 
 public class Tread {
     public static void main(String[] args) {
-        A obj1 = new A();
-        B obj2 = new B();
+        Runnable obj1 = new A();
+        Runnable obj2 = new B();
 
-        obj1.start();
-        obj2.start();
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
     }
 }
