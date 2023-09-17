df801333d086a621db6f46fc3f50c9c628ed40b2
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-17 03:48:09 +0200

    exceptions are run time errors

---
+++ telusko/ExceptionError.java
@@ -0,0 +1,19 @@
+// Exception errors are runtime erorr.
+
+public class ExceptionError {
+    public static void main(String[] args) {
+        int i = 4;
+        int j = 0;
+
+        try {
+            j = 18 / i;
+        }
+
+        catch (Exception e) {
+            System.out.println(e + "Sorry something went wrong");
+        }
+
+        System.out.println(j);
+        System.out.println("thanks for checking");
+    }
+}
