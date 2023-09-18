65fffa7938e6075629a5edf0d177787a67c1c2fd
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-18 20:46:47 +0200

    task 1

---
+++ Exercises/task_1/Me.java
@@ -0,0 +1,11 @@
+public class Me {
+
+    public static void sayName() {
+        System.out.println("my name is joshua");
+    }
+
+    public static void sayAge() {
+        System.out.println(16);
+    }
+
+}
