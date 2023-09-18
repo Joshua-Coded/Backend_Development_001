d6049baff79ba459a7d0a7397182e1763226e160
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-18 21:29:58 +0200

    task 3

---
+++ Exercises/constants/Constant.java
@@ -0,0 +1,6 @@
+public class Constant {
+    public static void main(String[] args) {
+        final String COMPANY_NAME = "African leadership university";
+        System.out.println(COMPANY_NAME);
+    }
+}
