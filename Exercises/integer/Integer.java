c9a2cbeca24f04e4923a986e857504728db9ee45
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-18 23:36:03 +0200

    task 4

---
+++ Exercises/integer/Integer.java
@@ -0,0 +1,15 @@
+public class Integer {
+    public static void main(String[] args) {
+
+        byte b1 = 5;
+        short s1 = 10;
+        int i1 = 20;
+        long l1 = 323;
+
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(i1);
+        System.out.println(l1);
+
+    }
+}
