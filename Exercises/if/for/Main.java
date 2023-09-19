c65c3ad7f9353adc9dfe9fd5bfed4096c4b07c7d
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-19 15:42:39 +0200

    more  task

---
+++ Exercises/if/for/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        for (; i <= 5;) {
+            System.out.println("Hello");
+            i++;
+        }
+
+        while (i <= 5) {
+            System.out.println("me and you");
+            i++;
+        }
+    }
+}
