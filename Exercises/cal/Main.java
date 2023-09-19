ff6217b4c13363ba86d3b19ab4919434fd4a00d7
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-19 13:41:51 +0200

    more  task

---
+++ Exercises/cal/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Enter 2 numbers: ( a + b ) ");
+
+        Scanner s = new Scanner(System.in);
+
+        double d1 = s.nextDouble();
+        char op = s.next().charAt(0);
+        double d2 = s.nextDouble();
+
+        if (op == '+') {
+            System.out.println((int) (d1 + d2));
+        } else if (op == '-') {
+            System.out.println((int) (d1 + d2));
+        } else if (op == '/') {
+            System.out.println((int) (d1 / d2));
+        }
+
+        else if (op == '*') {
+            System.out.println((int) (d1 * d2));
+        }
+
+    }
+}
