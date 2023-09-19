8204e50bcec5667817edb2b48786184b4fc041f3
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-19 11:26:22 +0200

    number task

---
+++ Exercises/number/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+       
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter  a number: ");
+        int n = s.nextInt();
+        System.out.println("Number: " + n);
+
+    }
+}
