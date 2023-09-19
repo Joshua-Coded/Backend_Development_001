3a00c1f3a8685bed7f44d4a7283c4aa035f7c3df
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-19 11:34:27 +0200

    more  task

---
+++ Exercises/scanner/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        if (input.hasNextInt()) {
+            System.out.println("you entered an integer");
+        }
+
+    }
+}
