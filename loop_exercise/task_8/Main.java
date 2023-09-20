35ea4a3f7eca7725bb29af784712f382c28d018a
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-20 11:18:59 +0200

    tasks

---
+++ loop_exercise/task_8/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "joshua aa";
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
+}
