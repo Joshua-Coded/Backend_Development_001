6d6ee8f7f3ee93d8588833351474ee8ccd26a698
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-21 16:31:00 +0200

    the inheritance of oop

---
+++ OOP/polymorphism/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        Circle circle = new Circle();
+        Triangle triangle = new Triangle();
+        Square sqaure = new Square();
+
+        // shape.area();
+        // circle.area();
+        // triangle.area();
+        sqaure.area();
+    }
+}
