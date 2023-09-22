839749339a1ae42b4a0a80772159abf6303c31df
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-22 14:34:18 +0200

    interface

---
+++ OOP/interfaces/Car.java
@@ -0,0 +1,23 @@
+public class Car implements Engine, Brake {
+
+    @Override
+    public void brake() {
+        System.out.println("I brake like a normal car");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("I start like a normal car");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("I stop like a normal car");
+    }
+
+    @Override
+    public void acc() {
+        System.out.println("I accelerate like a normal car");
+    }
+
+}
