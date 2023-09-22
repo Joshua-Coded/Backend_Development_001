9f30dbad593b2531e7c307d534488759fc922dfd
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-22 11:38:05 +0200

    computer networking

---
+++ OOP/access_controller/Access.java
@@ -0,0 +1,12 @@
+public class Access {
+    int num;
+    String name;
+    int[] arr;
+
+    // constructor
+    public Access(int num, String name) {
+        this.num = num;
+        this.name = name;
+        this.arr = new int[num];
+    }
+}
