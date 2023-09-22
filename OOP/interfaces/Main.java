662b71b615b441e7e490306708e38d52a6603218
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-22 14:36:37 +0200

    interface

---
+++ OOP/interfaces/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        car.acc();
+        car.start();
+        car.stop();
+        car.brakes();
+    }
+}
