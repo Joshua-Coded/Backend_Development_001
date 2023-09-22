b0f5a0fe462853979b0ea792ee3d676ca2e3f485
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-22 15:10:47 +0200

    interface

---
+++ OOP/interfaces/NiceCar.java
@@ -0,0 +1,28 @@
+public class NiceCar {
+    private Engine engine;
+    private Media player = new CdPlayer();
+
+    public NiceCar() {
+        engine = new PowerEngine();
+    }
+
+    public NiceCar(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void start() {
+        engine.start();
+    }
+
+    public void stop() {
+        engine.stop();
+    }
+
+    public void startMusic() {
+        player.start();
+    }
+
+    public void stopMusic() {
+        player.stop();
+    }
+}
