f68ecb9bff93052e88ff1f08200e02e9c0d213a6
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-25 22:01:58 +0200

    first java program ☑️

--- JAVA/kunal_java/first_java_program/Main.java
+++ JAVA/kunal_java/first_java_program/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -5,6 +6,14 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println(args[0]);
         System.out.print("thank you for learning java!");
+
+        // taking in data from users
+        // using the Scanner class
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number? ");
+        int n = s.nextInt();
+        System.out.println(n);
 
     }
 }
