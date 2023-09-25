9d9b5de9100950cb6b3f26976d2dc7a263b0cd4d
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-25 21:59:40 +0200

    first java program ☑️

--- JAVA/kunal_java/first_java_program/Main.java
+++ JAVA/kunal_java/first_java_program/Main.java
@@ -1,8 +1,10 @@
 public class Main {
-    // function collection of code ..
+
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
         System.out.println(args[0]);
+        System.out.print("thank you for learning java!");
 
     }
 }
