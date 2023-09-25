0c593596c466110748128b6b60949e8c555a906c
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-25 22:41:28 +0200

    taking in input from users☑️

---
+++ JAVA/kunal_java/first_java_program/Primitives.java
@@ -0,0 +1,15 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'a';
+        float marks = 23.5f;
+        double largeDecimalNumber = 3545.7;
+        long largeIntegerNumber = 5456576L;
+        boolean check  = false;
+        
+
+        // string is not primitives. it can not be broken further
+        String name = "Joshua Alana";
+
+    }
+}
