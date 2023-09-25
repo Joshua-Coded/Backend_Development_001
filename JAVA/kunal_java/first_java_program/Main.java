d183171681e12632647a75ab6b4d98a1042962c7
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-25 22:04:50 +0200

    taking in input from users☑️

--- JAVA/kunal_java/first_java_program/Main.java
+++ JAVA/kunal_java/first_java_program/Main.java
@@ -3,17 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.println(args[0]);
-        System.out.print("thank you for learning java!");
+        //System.out.println("Hello World!");
+        //System.out.println(args[0]);
+        //System.out.print("thank you for learning java!");
 
         // taking in data from users
         // using the Scanner class
 
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter a number? ");
+        System.out.print("Enter a number? ");
         int n = s.nextInt();
-        System.out.println(n);
+        System.out.println("Digit entered is: " + n);
 
     }
 }
