fe0c577e27ffad192b492b18e06d84220e826ee3
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-26 02:25:42 +0200

    linear search in 2d array☑️

--- JAVA/kunal_java/linear-search/SearchIn2DArray.java
+++ JAVA/kunal_java/linear-search/SearchIn2DArray.java
@@ -11,6 +11,8 @@ public class SearchIn2DArray {
         int target = 0;
         int[] ans = search(arr, target);
         System.out.println(Arrays.toString(ans));
+        System.out.println(max(arr));
+        System.out.println(min(arr));
     }
 
     // the method
@@ -23,6 +25,34 @@ public class SearchIn2DArray {
             }
         }
         return new int[] { -1, -1 };
+
+    }
+
+    // method for searching the max value in 2d array
+    static int max(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] > max) {
+                    max = arr[row][col];
+                }
+            }
+        }
+        return max;
+
+    }
+
+    // method for searching the min value in 2d array
+    static int min(int[][] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] < min) {
+                    min = arr[row][col];
+                }
+            }
+        }
+        return min;
 
     }
 
