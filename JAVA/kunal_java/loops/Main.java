5251345053363b8a59fe08a3de887227f9a9f8b7
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-07 19:34:57 +0200

    for loop ☑️

---
+++ JAVA/kunal_java/loops/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        /*
+         * syntax of for loop
+         * if (initialization; conditional; increment) {
+         * body
+         * }
+         */
+
+        // print numbers 1 - 5
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
