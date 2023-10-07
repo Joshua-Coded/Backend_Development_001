ed4d12d09b8beeb1940ab47057f84ca8e64b8510
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-07 19:26:24 +0200

    multiple if-else conditionals ☑️

---
+++ JAVA/kunal_java/classes_output/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args){
+        /*
+         * syntax of if statement 
+         * if (boolean expression T or F) {
+         *  body
+         * } else {
+         * do this
+         * }
+         * 
+         */
+        int salary = 2500;
+        // if (salary >= 1000){
+        //     salary += 2000;
+        // } else {
+        //     salary += 1000;
+        // }
+
+        // multiple if else statements
+        if (salary >= 1000) {
+            salary += 2000;
+        } else if (salary >= 2000) {
+            salary += 2000;
+        } else {
+            salary += 1000;
+        }
+        System.out.println("the new salary is " + salary);
+    }
+}
