440a89f4730279661502edbb3510d597c8c333e7
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-24 04:01:14 +0200

    primitive data types

---
+++ JAVA/CORE_JAVA/Variables/primitive_types/Area.java
@@ -0,0 +1,9 @@
+public class Area {
+    public static void main(String[] args) {
+        double r, pi, a;
+        r = 10.8;
+        pi = 3.142;
+        a = pi * r * r;
+        System.out.println("Area of a circle: " + a);
+    }
+}
