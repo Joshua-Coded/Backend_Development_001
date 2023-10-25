7250793f964a362bc49539e4455a771343c1f5bb
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-25 02:59:17 +0200

    bitwise

---
+++ JAVA/CORE_JAVA/Bit_wise/Operator.java
@@ -0,0 +1,7 @@
+public class Operator {
+    public static void main(String[] args) {
+        System.out.println(10 >> 2);
+        System.out.println(20 >> 2);
+        System.out.println(30 >> 3);
+    }
+}
