bf1998047d6582e7fa40d548a95adb5ac19a4646
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-31 13:21:07 +0200

    SWTICHes

---
+++ JAVA/CORE_JAVA/switches/StringsSwitch.java
@@ -0,0 +1,18 @@
+public class StringsSwitch {
+    public static void main(String[] args) {
+        String str = "two";
+        switch (str) {
+            case "one":
+                System.out.println("one");
+                break;
+            case "two":
+                System.out.println("two");
+                break;
+            case "three":
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("invalid string");
+        }
+    }
+}
