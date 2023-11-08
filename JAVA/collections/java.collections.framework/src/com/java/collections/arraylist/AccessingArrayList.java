2ba9e71bc5b1f22ff9de6f51437a8965d72b497a
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-08 12:35:46 +0200

    collections in java

--- JAVA/collections/java.collections.framework/src/com/java/collections/arraylist/AccessingArrayList.java
+++ JAVA/collections/java.collections.framework/src/com/java/collections/arraylist/AccessingArrayList.java
@@ -1,10 +1,30 @@
 package com.java.collections.arraylist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccessingArrayList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> topProramming =  new ArrayList<String>();
+		System.out.println("Is this a programming language: "  + topProramming.isEmpty());
 
+	topProramming.add("C");
+	topProramming.add("JAVA");
+	topProramming.add("PYTHON");
+	topProramming.add("JAVASCRIPT");
+	topProramming.add("C++");
+	
+	
+	System.out.println("Here are the top" + topProramming.size() + " " + "programming language in the world!!");
+	
+	String bestProgramming = topProramming.get(1);
+	
+	System.out.println("BestProgramming => " + bestProgramming);
+	
+	
+	topProramming.set(4, ".NET");
+	System.out.println(topProramming);
 	}
 
 }
