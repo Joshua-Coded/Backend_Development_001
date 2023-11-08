d986440fb52c170bb36de5c6467d544a88627108
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-08 14:35:38 +0200

    collections in java

--- JAVA/collections/java.collections.framework/src/com/java/collections/arraylist/IterateOverArrayList.java
+++ JAVA/collections/java.collections.framework/src/com/java/collections/arraylist/IterateOverArrayList.java
@@ -1,6 +1,7 @@
 package com.java.collections.arraylist;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class IterateOverArrayList {
@@ -12,6 +13,27 @@ public class IterateOverArrayList {
 			System.out.println(courses.get(i));
 		}
 		
+//		enhanced for each loop
+		for (String course : courses) {
+			System.out.println(course);
+		}
+		
+// for iterator  loop 
+		for (Iterator iterator = courses.iterator(); iterator.hasNext();) {
+			String course = (String) iterator.next();
+			System.out.println(course);
+			
+		}
+		Iterator<String> iterator = courses.iterator();
+		while (iterator.hasNext()) {
+			String course = (String) iterator.next();
+			System.out.println(course);
+		}
+//			java 8 stream  + lambdas example
+			courses.stream().forEach(course -> System.out.println(course));
+			
+//			java 8 foreach
+			courses.forEach((course) -> System.out.println(course));
+		}
 	}
 
-}
