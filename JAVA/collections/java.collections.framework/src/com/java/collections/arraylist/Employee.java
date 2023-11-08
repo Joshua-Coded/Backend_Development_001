8b6ea890a4dfcdbcbb0f342ac2a3a35ed14a3192
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-08 14:52:53 +0200

    collections in java

---
+++ JAVA/collections/java.collections.framework/src/com/java/collections/arraylist/Employee.java
@@ -0,0 +1,67 @@
+package com.java.collections.arraylist;
+
+public class Employee {
+	private int id;
+	private String name;
+	private int age;
+	private long salary;
+	
+	
+	public Employee(int id, String name, int age, long salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	/**
+	 * @return the salary
+	 */
+	public long getSalary() {
+		return salary;
+	}
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
