5af68f216162d92c7fec1fffc787d1457d78debc
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-08 21:21:45 +0200

    collections in java

---
+++ JAVA/collections/java.collections.framework/src/com/java/collections/linkedlist/CreateLinkedList.java
@@ -0,0 +1,15 @@
+package com.java.collections.linkedlist;
+
+import java.util.LinkedList;
+
+public class CreateLinkedList {
+	public static void main(String[] args) {
+		LinkedList<String> fruits = new LinkedList<String>();
+		fruits.add("Banana");
+		fruits.add("Apple");
+		fruits.add("Mango");
+		
+		
+		System.out.println(fruits);
+	}
+}
