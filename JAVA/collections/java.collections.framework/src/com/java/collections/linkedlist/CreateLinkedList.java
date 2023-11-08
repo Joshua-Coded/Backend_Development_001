5867f55ebc0580fd9964148492745104908e7242
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-08 21:32:45 +0200

    collections in java

--- JAVA/collections/java.collections.framework/src/com/java/collections/linkedlist/CreateLinkedList.java
+++ JAVA/collections/java.collections.framework/src/com/java/collections/linkedlist/CreateLinkedList.java
@@ -11,5 +11,22 @@ public class CreateLinkedList {
 		
 		
 		System.out.println(fruits);
+		
+		
+		// adding elements to a specific position in linkedlist
+		
+		fruits.add(2, "Watermelon");
+		fruits.add(3, "Pawpaw");
+		
+		System.out.println("Adding elements to a specific postion => " + fruits);
+		
+		
+		// adding elements at the beginning of linkedlist
+		fruits.addFirst("STRAWBERRY");
+		System.out.println("Adding at the first position +" + fruits);
+		
+		// adding at the end of linkedlist 
+		fruits.addLast("Orange");
+		System.out.println("Adding at the last position +" + fruits);
 	}
 }
