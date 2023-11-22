04b132c94ae6fe39e3067a2e42130297315a104c
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-22 22:52:17 +0200

    java

---
+++ JAVA/Java_guides/Student.java
@@ -0,0 +1,41 @@
+public class Student {
+    private String name;
+    private String college;
+
+    // constructor that returns the name of the student and its college
+
+    public Student(String name, String college) {
+        super();
+        this.name = name;
+        this.college = college;
+    }
+
+    // getters and setters for Student constructor
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    // the main method
+
+    public static void main(String[] args) throws Exception {
+        // use the student class to create objects
+        Student student = new Student("Joshua", "Alu");
+        Student student2 = new Student("Alana", "Alx");
+        Student student3 = new Student("Bami", "Ali");
+
+        System.out.println(student.getName() + " " + student.getCollege());
+    }
+}
