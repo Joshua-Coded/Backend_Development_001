70f515c450d95afed0ac1855af20a7766660f2fd
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-27 10:01:09 +0200

    moved the dynamic stack to it dir

---
+++ JAVA/SPRING-BOOT/springboot-demo/src/main/java/com/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,16 @@
+package com.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+		System.out.println("Hello Spring boot");
+
+}
+}
+
+	
